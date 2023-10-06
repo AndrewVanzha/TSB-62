3bf6a1ff0f570efc31306eaaecaae6b3d1e440cf
andrew1762 <a.vanzha@tsbnk.ru>
2023-10-06 17:47:07 +0300

    TSB-62 первоначальное размещение index.php и других файлов на странице valutny-kontrol

---
+++ home/bitrix/www/corporative-clients/bankovskoe-obsluzhivanie/valutny-kontrol/index_block_bottom.php
@@ -0,0 +1,48 @@
+<?/*?<div class="v21-container"><?*/?>
+    <section class="v21-popproducts-inyuans--wrap v21-section-interests">
+        <?/*?><h2 class="v21-h2 v21-block-interests--header">Популярные продукты</h2><?*/?>
+        <div class="v21-section-interests--box">
+            <div class="v21-section-interests--left">
+                <div class="v21-section-interests--textbox">
+                    <div class="v21-section-interests--texts">
+                        <h4>Таможенная карта Мир</h4>
+                        <p>Виртуальная карта для оплаты таможенных пошлин 24/7</p>
+                    </div>
+                    <div class="v21-section-interests--button">
+                        <a href="/corporative-clients/bankovskoe-obsluzhivanie/zarplatnyy-proekt/" class="v21-section-interests--textbox-link">Подробнее</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.26051 14L12.2174 3.04314V10.2501H14V0H3.74986V1.78263H10.9569L0 12.7395L1.26051 14Z" fill="white"/>
+                        </svg>
+                    </div>
+                </div>
+                <div class="v21-section-interests--imgbox">
+                    <img src="/corporative-clients/bankovskoe-obsluzhivanie/accounts-in-yuans/images/popprod_left_102.png" class="v21-section-interests--imgbox-img1" alt="картинка">
+                    <img src="/corporative-clients/bankovskoe-obsluzhivanie/accounts-in-yuans/images/popprod_left_176.png" class="v21-section-interests--imgbox-img2" alt="картинка">
+                    <img src="/corporative-clients/bankovskoe-obsluzhivanie/accounts-in-yuans/images/popprod_left_210.png" class="v21-section-interests--imgbox-img3" alt="картинка">
+                    <img src="/corporative-clients/bankovskoe-obsluzhivanie/accounts-in-yuans/images/popprod_left_250.png" class="v21-section-interests--imgbox-img4" alt="картинка">
+                </div>
+            </div>
+            <div class="v21-section-interests--right">
+                <div class="v21-section-interests--textbox">
+                    <div class="v21-section-interests--texts">
+                        <h4>Конверсионные сделки</h4>
+                        <p>Покупка/продажа иностранной валюты по курсу близкому к биржевому, возможность хеджирования</p>
+                    </div>
+                    <div class="v21-section-interests--button">
+                        <a href="/corporative-clients/bankovskoe-obsluzhivanie/ekvayring/" class="v21-section-interests--textbox-link">Подробнее</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.26051 14L12.2174 3.04314V10.2501H14V0H3.74986V1.78263H10.9569L0 12.7395L1.26051 14Z" fill="white"/>
+                        </svg>
+                    </div>
+                    <?/*?><button><a href="tel:88005053773" class="">Позвоните 8 (800) 505-37-73</a></button><?*/?>
+                </div>
+                <div class="v21-section-interests--imgbox">
+                    <img src="/corporative-clients/bankovskoe-obsluzhivanie/accounts-in-yuans/images/popprod_right_102.png" class="v21-section-interests--imgbox-img1" alt="картинка">
+                    <img src="/corporative-clients/bankovskoe-obsluzhivanie/accounts-in-yuans/images/popprod_right_176.png" class="v21-section-interests--imgbox-img2" alt="картинка">
+                    <img src="/corporative-clients/bankovskoe-obsluzhivanie/accounts-in-yuans/images/popprod_right_210.png" class="v21-section-interests--imgbox-img3" alt="картинка">
+                    <img src="/corporative-clients/bankovskoe-obsluzhivanie/accounts-in-yuans/images/popprod_right_250.png" class="v21-section-interests--imgbox-img4" alt="картинка">
+                </div>
+            </div>
+        </div>
+    </section>
+<?php/*?></div><?php*/?>
