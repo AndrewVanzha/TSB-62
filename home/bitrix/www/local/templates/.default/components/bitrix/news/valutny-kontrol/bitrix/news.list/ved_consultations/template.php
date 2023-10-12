9de1edfa88a8959bd06dabdbad65458eaac29ee7
andrew1762 <a.vanzha@tsbnk.ru>
2023-10-12 13:48:21 +0300

    tsb-62 верстка 5го блока ved_consultations

--- home/bitrix/www/local/templates/.default/components/bitrix/news/valutny-kontrol/bitrix/news.list/ved_consultations/template.php
+++ home/bitrix/www/local/templates/.default/components/bitrix/news/valutny-kontrol/bitrix/news.list/ved_consultations/template.php
@@ -15,30 +15,24 @@ $this->setFrameMode(true);
 <? //debugg($arParams["SERVICES_BLOCK"]) ?>
 <? //debugg($arResult["PROPERTIES"]) ?>
 <??>
-<section class="consultations-tileblock">
-    <h3 class="consultations-tileblock__header"><?= $arResult["PROPERTY_HEADER"] ?></h3>
-    <div class="consultations-tileblock__grid">
-        <?/*?><div class="base-account-tileblock__grid--horline horline-1"></div>
-        <div class="base-account-tileblock__grid--horline horline-2"></div><?*/?>
-        <? foreach ($arResult["PROPERTIES"][$arParams['SERVICES_BLOCK'][0]] as $key=>$arItem) : ?>
-            <div class="consultations-tileblock__grid--item">
-                <div class="consultations-tileblock__grid--item-box box-1">
-                    <div class="consultations-tileblock__grid--img">
-                        <img
-                                src="<?=CFile::GetPath($arItem["icon"])?>"
-                                alt="иконка"
-                                title="<?=$arItem["main"]?>"
-                        />
-                    </div>
-                    <h4 class="consultations-tileblock__grid--title"><?= $arItem["main"]; ?></h4>
-                    <p class="consultations-tileblock__grid--subtitle">
-                        <span><?= $arItem["dop"]; ?></span>
+    <section class="ved-consultation-tileblock">
+        <?/*?><h3 class="ved-consultation-tileblock__header"><?= $arResult["PROPERTY_HEADER"]; ?></h3><?*/?>
+        <div class="ved-consultation-tileblock__grid">
+            <?/*?><div class="ved-consultation-tileblock__grid--horline horline-1"></div>
+        <div class="ved-consultation-tileblock__grid--horline horline-2"></div><?*/?>
+            <?/*?><?*/?>
+            <? foreach ($arResult["PROPERTIES"][$arParams['SERVICES_BLOCK'][0]] as $key=>$arItem) : ?>
+                <div class="ved-consultation-tileblock__grid--item" >
+                    <p class="ved-consultation-tileblock__grid--bird">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="12" viewBox="0 0 16 12" fill="none">
+                            <path d="M5.66129 11.4147L0.995392 6.7488C0.721795 6.4752 0.721794 6.03161 0.995392 5.75802C1.26899 5.48442 1.71258 5.48442 1.98618 5.75802L5.66129 9.43313L14.0138 1.0806C14.2874 0.807 14.731 0.806999 15.0046 1.0806C15.2782 1.35419 15.2782 1.79778 15.0046 2.07138L5.66129 11.4147Z" fill="#00345E"/>
+                        </svg>
                     </p>
+                    <p class="ved-consultation-tileblock__grid--title"><?= $arItem["main"]; ?></p>
                 </div>
-            </div>
-        <? endforeach; ?>
-    </div>
-</section>
+            <? endforeach; ?>
+        </div>
+    </section>
 <??>
 <?php/*?>
 <script>
