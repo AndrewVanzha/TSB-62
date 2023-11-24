0c2fa2859346a285a2cdc8d250213bd448534225
andrew1762 <a.vanzha@tsbnk.ru>
2023-11-24 19:29:49 +0300

    tsb-62 1я редакция /templates/.default/components/bitrix/news/komission-tariffs-rko/news.php

--- home/bitrix/www/local/templates/.default/components/bitrix/news/komission-tariffs-rko/news.php
+++ home/bitrix/www/local/templates/.default/components/bitrix/news/komission-tariffs-rko/news.php
@@ -53,9 +53,11 @@ $this->setFrameMode(true);
 <?endif?>
 
 <?
-Включен API инфоблока Валютный контроль ValutnyKontrol
+Включен API инфоблока Валютный контроль RkoTariffs
 */?>
-<? $parent_section = 593;  // вариант ноября 2023
+<?
+$parent_section = [601];  // Общая информация
+$services_block = [10716]; // Условия предоставления пакетов
 $APPLICATION->IncludeComponent(
 	"bitrix:news.list",
 	"komission-tariff_top",
@@ -63,7 +65,8 @@ $APPLICATION->IncludeComponent(
 		"ADD_ELEMENT_CHAIN" => $arParams["ADD_ELEMENT_CHAIN"],
 		"ADD_SECTIONS_CHAIN" => $arParams["ADD_SECTIONS_CHAIN"],
 		"INCLUDE_IBLOCK_INTO_CHAIN" => $arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
-		//"PARENT_SECTION" => $parent_section,
+		"PARENT_SECTION" => $parent_section,
+        "SERVICES_BLOCK" => $services_block,
 		"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
 		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
 		"NEWS_COUNT" => $arParams["NEWS_COUNT"],
@@ -112,6 +115,125 @@ $APPLICATION->IncludeComponent(
 	),
 	$component
 );?>
+<?
+$parent_section = [594, 595, 596];  // Пакеты
+$APPLICATION->IncludeComponent(
+    "bitrix:news.list",
+    "komission-tariff_table",
+    Array(
+        "ADD_ELEMENT_CHAIN" => $arParams["ADD_ELEMENT_CHAIN"],
+        "ADD_SECTIONS_CHAIN" => $arParams["ADD_SECTIONS_CHAIN"],
+        "INCLUDE_IBLOCK_INTO_CHAIN" => $arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
+        "PARENT_SECTION" => $parent_section,
+        "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+        "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+        "NEWS_COUNT" => $arParams["NEWS_COUNT"],
+        "SORT_BY1" => $arParams["SORT_BY1"],
+        "SORT_ORDER1" => $arParams["SORT_ORDER1"],
+        "SORT_BY2" => $arParams["SORT_BY2"],
+        "SORT_ORDER2" => $arParams["SORT_ORDER2"],
+        "FIELD_CODE" => $arParams["LIST_FIELD_CODE"],
+        "PROPERTY_CODE" => $arParams["LIST_PROPERTY_CODE"],
+        "DETAIL_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
+        "SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
+        "IBLOCK_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"],
+        "DISPLAY_PANEL" => $arParams["DISPLAY_PANEL"],
+        "SET_TITLE" => $arParams["SET_TITLE"],
+        "SET_LAST_MODIFIED" => $arParams["SET_LAST_MODIFIED"],
+        "MESSAGE_404" => $arParams["MESSAGE_404"],
+        "SET_STATUS_404" => $arParams["SET_STATUS_404"],
+        "SHOW_404" => $arParams["SHOW_404"],
+        "FILE_404" => $arParams["FILE_404"],
+        "CACHE_TYPE" => $arParams["CACHE_TYPE"],
+        "CACHE_TIME" => $arParams["CACHE_TIME"],
+        "CACHE_FILTER" => $arParams["CACHE_FILTER"],
+        "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+        "DISPLAY_TOP_PAGER" => $arParams["DISPLAY_TOP_PAGER"],
+        "DISPLAY_BOTTOM_PAGER" => $arParams["DISPLAY_BOTTOM_PAGER"],
+        "PAGER_TITLE" => $arParams["PAGER_TITLE"],
+        "PAGER_TEMPLATE" => $arParams["PAGER_TEMPLATE"],
+        "PAGER_SHOW_ALWAYS" => $arParams["PAGER_SHOW_ALWAYS"],
+        "PAGER_DESC_NUMBERING" => $arParams["PAGER_DESC_NUMBERING"],
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => $arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
+        "PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
+        "PAGER_BASE_LINK_ENABLE" => $arParams["PAGER_BASE_LINK_ENABLE"],
+        "PAGER_BASE_LINK" => $arParams["PAGER_BASE_LINK"],
+        "PAGER_PARAMS_NAME" => $arParams["PAGER_PARAMS_NAME"],
+        "DISPLAY_DATE" => $arParams["DISPLAY_DATE"],
+        "DISPLAY_NAME" => "Y",
+        "DISPLAY_PICTURE" => $arParams["DISPLAY_PICTURE"],
+        "DISPLAY_PREVIEW_TEXT" => $arParams["DISPLAY_PREVIEW_TEXT"],
+        "PREVIEW_TRUNCATE_LEN" => $arParams["PREVIEW_TRUNCATE_LEN"],
+        "ACTIVE_DATE_FORMAT" => $arParams["LIST_ACTIVE_DATE_FORMAT"],
+        "USE_PERMISSIONS" => $arParams["USE_PERMISSIONS"],
+        "GROUP_PERMISSIONS" => $arParams["GROUP_PERMISSIONS"],
+        "FILTER_NAME" => $arParams["FILTER_NAME"],
+        "HIDE_LINK_WHEN_NO_DETAIL" => $arParams["HIDE_LINK_WHEN_NO_DETAIL"],
+        "CHECK_DATES" => $arParams["CHECK_DATES"],
+    ),
+    $component
+);?>
+<?
+$parent_section = [601];  // Общая информация
+$services_block = [10718]; // Примечания
+$APPLICATION->IncludeComponent(
+    "bitrix:news.list",
+    "komission-tariff_footnotes",
+    Array(
+        "ADD_ELEMENT_CHAIN" => $arParams["ADD_ELEMENT_CHAIN"],
+        "ADD_SECTIONS_CHAIN" => $arParams["ADD_SECTIONS_CHAIN"],
+        "INCLUDE_IBLOCK_INTO_CHAIN" => $arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
+        "PARENT_SECTION" => $parent_section,
+        "SERVICES_BLOCK" => $services_block,
+        "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+        "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+        "NEWS_COUNT" => $arParams["NEWS_COUNT"],
+        "SORT_BY1" => $arParams["SORT_BY1"],
+        "SORT_ORDER1" => $arParams["SORT_ORDER1"],
+        "SORT_BY2" => $arParams["SORT_BY2"],
+        "SORT_ORDER2" => $arParams["SORT_ORDER2"],
+        "FIELD_CODE" => $arParams["LIST_FIELD_CODE"],
+        "PROPERTY_CODE" => $arParams["LIST_PROPERTY_CODE"],
+        "DETAIL_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
+        "SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
+        "IBLOCK_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"],
+        "DISPLAY_PANEL" => $arParams["DISPLAY_PANEL"],
+        "SET_TITLE" => $arParams["SET_TITLE"],
+        "SET_LAST_MODIFIED" => $arParams["SET_LAST_MODIFIED"],
+        "MESSAGE_404" => $arParams["MESSAGE_404"],
+        "SET_STATUS_404" => $arParams["SET_STATUS_404"],
+        "SHOW_404" => $arParams["SHOW_404"],
+        "FILE_404" => $arParams["FILE_404"],
+        "CACHE_TYPE" => $arParams["CACHE_TYPE"],
+        "CACHE_TIME" => $arParams["CACHE_TIME"],
+        "CACHE_FILTER" => $arParams["CACHE_FILTER"],
+        "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+        "DISPLAY_TOP_PAGER" => $arParams["DISPLAY_TOP_PAGER"],
+        "DISPLAY_BOTTOM_PAGER" => $arParams["DISPLAY_BOTTOM_PAGER"],
+        "PAGER_TITLE" => $arParams["PAGER_TITLE"],
+        "PAGER_TEMPLATE" => $arParams["PAGER_TEMPLATE"],
+        "PAGER_SHOW_ALWAYS" => $arParams["PAGER_SHOW_ALWAYS"],
+        "PAGER_DESC_NUMBERING" => $arParams["PAGER_DESC_NUMBERING"],
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => $arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
+        "PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
+        "PAGER_BASE_LINK_ENABLE" => $arParams["PAGER_BASE_LINK_ENABLE"],
+        "PAGER_BASE_LINK" => $arParams["PAGER_BASE_LINK"],
+        "PAGER_PARAMS_NAME" => $arParams["PAGER_PARAMS_NAME"],
+        "DISPLAY_DATE" => $arParams["DISPLAY_DATE"],
+        "DISPLAY_NAME" => "Y",
+        "DISPLAY_PICTURE" => $arParams["DISPLAY_PICTURE"],
+        "DISPLAY_PREVIEW_TEXT" => $arParams["DISPLAY_PREVIEW_TEXT"],
+        "PREVIEW_TRUNCATE_LEN" => $arParams["PREVIEW_TRUNCATE_LEN"],
+        "ACTIVE_DATE_FORMAT" => $arParams["LIST_ACTIVE_DATE_FORMAT"],
+        "USE_PERMISSIONS" => $arParams["USE_PERMISSIONS"],
+        "GROUP_PERMISSIONS" => $arParams["GROUP_PERMISSIONS"],
+        "FILTER_NAME" => $arParams["FILTER_NAME"],
+        "HIDE_LINK_WHEN_NO_DETAIL" => $arParams["HIDE_LINK_WHEN_NO_DETAIL"],
+        "CHECK_DATES" => $arParams["CHECK_DATES"],
+    ),
+    $component
+);?>
+
     </div><!-- v21-container -->
 </div><!-- v21-section -->
 
@@ -121,7 +243,7 @@ $APPLICATION->IncludeComponent(
             <h3 class="v21-ved-consultation-header">Закажите бесплатную консультацию по законодательству</h3>
         </div>
         <div class="v21-container v21-ved-consultations-wrap">tariff_consultations
-            <? $services_block = [10647]; // Закажите бесплатную консультацию по законодательству
+            <? /*$services_block = [10647]; // Закажите бесплатную консультацию по законодательству
             $APPLICATION->IncludeComponent(
                 "bitrix:news.list",
                 "tariff_consultations",
@@ -178,7 +300,7 @@ $APPLICATION->IncludeComponent(
                     "CHECK_DATES" => $arParams["CHECK_DATES"],
                 ),
                 $component
-            );?>
+            );*/?>
             <div class="v21-consult-application" id="fCurrencyForm fConsultForm">
                 <? $APPLICATION->IncludeComponent(
                     "webtu:feedback",
