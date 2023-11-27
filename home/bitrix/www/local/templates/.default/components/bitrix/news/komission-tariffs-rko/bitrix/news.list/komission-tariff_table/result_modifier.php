4f0ef8e93e76e94025c3c8f68b108e7043cd6e04
andrew1762 <a.vanzha@tsbnk.ru>
2023-11-27 20:00:20 +0300

    tsb-62 составление логики шаблона/komission-tariff_top/

--- home/bitrix/www/local/templates/.default/components/bitrix/news/komission-tariffs-rko/bitrix/news.list/komission-tariff_table/result_modifier.php
+++ home/bitrix/www/local/templates/.default/components/bitrix/news/komission-tariffs-rko/bitrix/news.list/komission-tariff_table/result_modifier.php
@@ -1,5 +1,55 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$topSectionList = [];
+$sectionList = [];
+$rs_section = \Bitrix\Iblock\SectionTable::getList([
+    'select' => [
+        'ID',
+        'CODE',
+        'NAME',
+        'DESCRIPTION',
+        //'IBLOCK_ID',
+        'IBLOCK_SECTION_ID',
+        'DEPTH_LEVEL',
+        'SECTION_PAGE_URL' => 'IBLOCK.SECTION_PAGE_URL',
+        //'IBLOCK_NAME' => 'IBLOCK.NAME',
+    ],
+    'filter' => [
+        'IBLOCK_ID' => $arParams["IBLOCK_ID"],
+        'DEPTH_LEVEL' => [1, 2],
+        'ACTIVE' => "Y",
+        //'ID' => $arParams["PARENT_SECTION"]
+    ],
+    'order' => [
+        'IBLOCK_SECTION_ID' => 'ASC',
+    ],
+]);
+while ($ar_section=$rs_section->fetch()) {
+    if ($ar_section['DEPTH_LEVEL'] == 1) {
+        $topSectionList[] = [
+            'ID' => $ar_section['ID'],
+            'CODE' => $ar_section['CODE'],
+            'NAME' => $ar_section['NAME'],
+            'DESCRIPTION' => $ar_section['DESCRIPTION'],
+            'IBLOCK_SECTION_ID' => $ar_section['IBLOCK_SECTION_ID'],
+            'DEPTH_LEVEL' => $ar_section['DEPTH_LEVEL'],
+            'SECTION_PAGE_URL' => \CIBlock::ReplaceDetailUrl($ar_section['SECTION_PAGE_URL'], $ar_section, true, 'S'),
+        ];
+    } else {
+        $sectionList[] = [
+            'ID' => $ar_section['ID'],
+            'CODE' => $ar_section['CODE'],
+            'NAME' => $ar_section['NAME'],
+            'DESCRIPTION' => $ar_section['DESCRIPTION'],
+            'IBLOCK_SECTION_ID' => $ar_section['IBLOCK_SECTION_ID'],
+            'DEPTH_LEVEL' => $ar_section['DEPTH_LEVEL'],
+            'SECTION_PAGE_URL' => \CIBlock::ReplaceDetailUrl($ar_section['SECTION_PAGE_URL'], $ar_section, true, 'S'),
+        ];
+    }
+}
+debugg($topSectionList);
+debugg($sectionList);
 /*
 debugg($arParams['PARENT_SECTION']);
 debugg($arParams["IBLOCK_ID"]);
@@ -25,58 +75,30 @@ while ($sectionProp = $secRes->GetNext()) {
 
 debugg($arSection);
 */
-//debugg($arParams);
+// Получить массив подсекций
+/*
+$ar_sub_sections = [];
+foreach ($arResult['ITEMS'] as $arItem) {
+    $ar_sub_sections[] = $arItem['IBLOCK_SECTION_ID'];
+}
+$arResult['AR_SECTIONS'] = array_unique($ar_sub_sections);
+function compwrds($a, $b) {
+    return strnatcmp($a["name"], $b["name"]);
+}
+usort($arResult['AR_SECTIONS'], "compwrds");
+//debugg($arResult['AR_SECTIONS']);
+*/
+for ($ii=0; $ii<count($sectionList); $ii++) {
+    for ($key=0; $key<count($arResult['ITEMS']); $key++) {
+        if ($arResult['ITEMS'][$key]['IBLOCK_SECTION_ID'] == $sectionList[$ii]['ID']) {
+            $sectionList[$ii]['ITEMS'][$arResult['ITEMS'][$key]['ID']] = $arResult['ITEMS'][$key];
+        }
+    }
+}
+//debugg($sectionList);
+$arResult['TABLE'] = $sectionList;
+$arResult['TOP_SECTION'] = $topSectionList;
+//debugg($arResult['TABLE']);
 //debugg($arResult);
-
-//$arResult['PROPERTIES'] = [];
-$main_items = [];
-$dop_items = [];
-$icon_items = [];
-
-foreach ($arResult["ITEMS"] as $arItem) {
-    if ($arItem['ID'] == $arParams['SERVICES_BLOCK'][0]) {
-        //debugg($arParams['SERVICES_BLOCK'][0]);
-        //debugg($arItem['PROPERTIES']['ATT_SERVICES']);
-        $arResult['PROPERTY_HEADER'] = $arItem['~NAME'];
-        if ($arItem['PROPERTIES']['ATT_SERVICES']['VALUE']) {
-            foreach ($arItem['PROPERTIES']['ATT_SERVICES']['~VALUE'] as $item) {
-                $main_items[] = $item;
-            }
-        }
-        if ($arItem['PROPERTIES']['ATT_SERVICES']['DESCRIPTION']) {
-            foreach ($arItem['PROPERTIES']['ATT_SERVICES']['DESCRIPTION'] as $item) {
-                $dop_items[] = $item;
-            }
-        }
-        if ($arItem['PROPERTIES']['ATT_SERVICES_ICONS']['VALUE']) {
-            foreach ($arItem['PROPERTIES']['ATT_SERVICES_ICONS']['VALUE'] as $item) {
-                $icon_items[] = $item;
-            }
-        }
-        $arResult['NOTES'][$arParams['SERVICES_BLOCK'][0]] = $arItem['PROPERTIES']['ATT_NOTES']['VALUE'];
-    }
-}
-//debugg($main_items);
-//debugg($dop_items);
-//debugg($icon_items);
-
-if (!empty($main_items)) {
-    for ($ii=0; $ii<count($main_items); $ii++) {
-        $arResult['PROPERTIES'][$arParams['SERVICES_BLOCK'][0]][$ii]['main'] = $main_items[$ii];
-        if ($dop_items[$ii]) {
-            $arResult['PROPERTIES'][$arParams['SERVICES_BLOCK'][0]][$ii]['dop'] = $dop_items[$ii];
-        } else {
-            $arResult['PROPERTIES'][$arParams['SERVICES_BLOCK'][0]][$ii]['dop'] = '';
-        }
-        if ($icon_items[$ii]) {
-            $arResult['PROPERTIES'][$arParams['SERVICES_BLOCK'][0]][$ii]['icon'] = $icon_items[$ii];
-        } else {
-            $arResult['PROPERTIES'][$arParams['SERVICES_BLOCK'][0]][$ii]['icon'] = '';
-        }
-    }
-}
-
-//debugg($arResult['PROPERTIES']);
-unset($main_items);
-unset($dop_items);
-unset($icon_items);
+//debugg($arResult['ITEMS']);
+unset($sectionList);
--- home/bitrix/www/local/templates/.default/components/bitrix/news/komission-tariffs-rko/bitrix/news.list/komission-tariff_table/template.php
+++ home/bitrix/www/local/templates/.default/components/bitrix/news/komission-tariffs-rko/bitrix/news.list/komission-tariff_table/template.php
@@ -12,14 +12,200 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<? //debugg($arParams["SERVICES_BLOCK"]) ?>
-<? //debugg($arResult["PROPERTIES"]) ?>
-<??>
+<? //debugg($arParams["SERVICES_BLOCK"]); ?>
+<?// debugg($arResult); ?>
+<? //debugg($arResult["PROPERTIES"]); ?>
+
+<div class="template-topblock">
+    <div class="template-topblock__block">
+        <h1 class="template-topblock__header">Тарифы комиссионного вознаграждения за пакеты услуг расчетно-кассового обслуживания счетов юридических лиц (за исключением кредитных организаций), индивидуальных предпринимателей и физических лиц, занимающихся в установленном законодательством российской федерации порядке частной практикой, в АКБ «ТРАНССТРОЙБАНК» (АО)</h1>
+
+        <h2>УТВЕРЖДЕНЫ<br>
+            Приказом<br>
+            Председателя Правления<br>
+            АКБ «Трансстройбанк» (АО)<br>
+            № 422 от «12» августа 2021 г.<br>
+            Вступают в силу с «01» сентября 2021 г.</h2>
+        <h3 class="template-topblock__content">Условия предоставления пакетов</h3>
+        <div class="template-topblock__buttons">
+            <a href="#fTariffConsultForm" class="v21-button-2022 template-topblock__button button-1 js-tariff-table__button">
+                <span>Получить консультацию</span>
+            </a>
+        </div>
+    </div>
+    <div class="template-topblock__image template-topblock__image--1366">
+        <img
+                src="<?=CFile::GetPath($arResult['PICTURE'])?>"
+                alt="картинка"
+                title="<?=$arResult["NAME"]?>"
+        />
+    </div>
+    <?if($arParams["DISPLAY_PICTURE"]!="N" && $arResult['PICTURE']):?>
+        <? if ($arResult["SECTION"]["PATH"][1366]) : ?>
+            <div class="template-topblock__image template-topblock__image--1366">
+                <img
+                        src="<?=CFile::GetPath($arResult["SECTION"]["PATH"][1366]["PICTURE"])?>"
+                        alt="картинка"
+                        title="<?=$arResult["NAME"]?>"
+                />
+            </div>
+        <? endif; ?>
+        <? if ($arResult["SECTION"]["PATH"][1024]) : ?>
+            <div class="template-topblock__image template-topblock__image--1024">
+                <img
+                        src="<?=CFile::GetPath($arResult["SECTION"]["PATH"][1024]["PICTURE"])?>"
+                        alt="картинка"
+                        title="<?=$arResult["NAME"]?>"
+                />
+            </div>
+        <? endif; ?>
+        <? if ($arResult["SECTION"]["PATH"][768]) : ?>
+            <div class="template-topblock__image template-topblock__image--768">
+                <img
+                        src="<?=CFile::GetPath($arResult["SECTION"]["PATH"][768]["PICTURE"])?>"
+                        alt="картинка"
+                        title="<?=$arResult["NAME"]?>"
+                />
+            </div>
+        <? endif; ?>
+        <? if ($arResult["SECTION"]["PATH"][480]) : ?>
+            <div class="template-topblock__image template-topblock__image--480">
+                <img
+                        src="<?=CFile::GetPath($arResult["SECTION"]["PATH"][480]["PICTURE"])?>"
+                        alt="картинка"
+                        title="<?=$arResult["NAME"]?>"
+                />
+            </div>
+        <? endif; ?>
+        <? if ($arResult["SECTION"]["PATH"][320]) : ?>
+            <div class="template-topblock__image template-topblock__image--320">
+                <img
+                        src="<?=CFile::GetPath($arResult["SECTION"]["PATH"][320]["PICTURE"])?>"
+                        alt="картинка"
+                        title="<?=$arResult["NAME"]?>"
+                />
+            </div>
+        <? endif; ?>
+    <?endif?>
+    <p class="template-topblock__content"><?echo $arResult["~DESCRIPTION"]; ?></p>
+</div>
+
+<script>
+    $(document).ready(function () {
+        $('.js-tariff-table__button').on('click', function() {
+            let href = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(href).offset().top - 120
+            }, {
+                duration: 800,   // по умолчанию «400»
+                easing: "linear" // по умолчанию «swing»
+            });
+            return false;
+        });
+    });
+</script>
+
+<div>
+    <? foreach ($arResult['TOP_SECTION'] as $section) : ?>
+        <a href="<?= $section['SECTION_PAGE_URL'] ?>"><?= $section['DESCRIPTION'] ?></a>
+    <? endforeach; ?>
+</div>
+
+<div>
+    <div>№ п/п</div>
+    <div>Вид операций и услуг</div>
+    <div>Пакет<br><? echo $arResult['SECTION']['PATH'][0]['~DESCRIPTION']; ?></div>
+</div>
+<? foreach ($arResult['TABLE'] as $key=>$arItems) : ?>
+    <div>
+        <div><?=$key+1?>.</div>
+        <div><?= $arItems['DESCRIPTION'] ?></div>
+        <div></div>
+        <? foreach ($arItems['ITEMS'] as $arItem) : ?>
+            <div><?= $arItem['PROPERTIES']['ATT_TABLE_LINE']['VALUE']; ?></div>
+            <? if ($arItem['PROPERTIES']['ATT_SERVICE_POINT']['VALUE']) : ?>
+                <div><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT']['~VALUE']; ?></div>
+                <div><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT']['~DESCRIPTION']; ?></div>
+            <? endif; ?>
+            <? if ($arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['VALUE']) : ?>
+                <? for ($kk=0; $kk<count($arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['~VALUE']); $kk++) : ?>
+                    <div><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['~VALUE'][$kk]; ?></div>
+                    <div><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['~DESCRIPTION'][$kk]; ?></div>
+                <? endfor; ?>
+            <? endif; ?>
+        <? endforeach; ?>
+    </div>
+<? endforeach; ?>
+
+<?foreach($arResult["ITEMS"] as $arItem):?>
+    <?
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
+<div class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+    <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+        <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+            <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
+                        class="preview_picture"
+                        border="0"
+                        src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                        width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+                        height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+                        alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                        title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                        style="float:left"
+                /></a>
+        <?else:?>
+            <img
+                    class="preview_picture"
+                    border="0"
+                    src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                    width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+                    height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+                    alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                    title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                    style="float:left"
+            />
+        <?endif;?>
+    <?endif?>
+    <?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
+        <span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
+    <?endif?>
+    <?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
+        <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+            <a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br />
+        <?else:?>
+            <b><?echo $arItem["NAME"]?></b><br />
+        <?endif;?>
+    <?endif;?>
+    <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
+        <?echo $arItem["PREVIEW_TEXT"];?>
+    <?endif;?>
+    <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+        <div style="clear:both"></div>
+    <?endif?>
+    <?foreach($arItem["FIELDS"] as $code=>$value):?>
+        <small>
+            <?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
+        </small><br />
+    <?endforeach;?>
+    <?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+        <small>
+            <?=$arProperty["NAME"]?>:&nbsp;
+            <?if(is_array($arProperty["DISPLAY_VALUE"])):?>
+                <?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
+            <?else:?>
+                <?=$arProperty["DISPLAY_VALUE"];?>
+            <?endif?>
+        </small><br />
+    <?endforeach;?>
+    </div>
+<?endforeach;?>
+
+<?/*?>
 <section class="consultations-tileblock">
     <h3 class="consultations-tileblock__header"><?= $arResult["PROPERTY_HEADER"] ?></h3>
     <div class="consultations-tileblock__grid">
-        <?/*?><div class="base-account-tileblock__grid--horline horline-1"></div>
-        <div class="base-account-tileblock__grid--horline horline-2"></div><?*/?>
         <? foreach ($arResult["PROPERTIES"][$arParams['SERVICES_BLOCK'][0]] as $key=>$arItem) : ?>
             <div class="consultations-tileblock__grid--item">
                 <div class="consultations-tileblock__grid--item-box item-box-<?= $key ?>">
@@ -41,8 +227,8 @@ $this->setFrameMode(true);
         <? endforeach; ?>
     </div>
 </section>
+<?*/?>
 <??>
-<?php/*?>
 <script>
     $(document).ready(function () {
         $('.js-base-account__button').on('click', function() {
@@ -62,4 +248,4 @@ $this->setFrameMode(true);
         );
     });
 </script>
-<?php*/?>
+<??>
