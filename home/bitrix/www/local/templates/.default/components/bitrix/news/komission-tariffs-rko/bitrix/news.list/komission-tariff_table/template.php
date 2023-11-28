028e11d50957bbb98dee1005b08c39d70689f525
andrew1762 <a.vanzha@tsbnk.ru>
2023-11-28 18:57:11 +0300

    tsb-62 верстка шаблона /komission-tariff_table/ подход 2

--- home/bitrix/www/local/templates/.default/components/bitrix/news/komission-tariffs-rko/bitrix/news.list/komission-tariff_table/template.php
+++ home/bitrix/www/local/templates/.default/components/bitrix/news/komission-tariffs-rko/bitrix/news.list/komission-tariff_table/template.php
@@ -18,22 +18,14 @@ $this->setFrameMode(true);
 
 <div class="template-topblock">
     <div class="template-topblock__block">
-        <!--h1 class="template-topblock__header">Тарифы комиссионного вознаграждения за пакеты услуг расчетно-кассового обслуживания счетов юридических лиц (за исключением кредитных организаций), индивидуальных предпринимателей и физических лиц, занимающихся в установленном законодательством российской федерации порядке частной практикой, в АКБ «ТРАНССТРОЙБАНК» (АО)</h1-->
         <h1 class="template-topblock__header"><?= $arResult['COMMON_SECTION']['ITEMS'][10758]['PREVIEW_TEXT']; ?></h1>
         <h2 class="template-topblock__anno"><?= $arResult['COMMON_SECTION']['ITEMS'][10757]['PREVIEW_TEXT']; ?></h2>
-        <?/*?><h2 class="template-topblock__anno">УТВЕРЖДЕНЫ<br>
-            Приказом<br>
-            Председателя Правления<br>
-            АКБ «Трансстройбанк» (АО)<br>
-            № 422 от «12» августа 2021 г.<br>
-            Вступают в силу с «01» сентября 2021 г.</h2><?*/?>
         <div class="template-topblock__buttons">
-            <a href="#fTariffConsultForm" class="v21-button-2022 template-topblock__button button-1 js-tariff-table__button">
+            <a href="#fBusinessAccountForm" class="v21-button-2022 template-topblock__button button-1 js-tariff-table__button">
                 <span>Получить консультацию</span>
             </a>
         </div>
     </div>
-    <!--h3 class="template-topblock__subheader">Условия предоставления пакетов</h3-->
     <h3 class="template-topblock__subheader"><?= $arResult['COMMON_SECTION']['ITEMS'][10716]['SUBHEADERVALUE']; ?></h3>
 
     <div class="template-topblock__image">
@@ -66,7 +58,6 @@ $this->setFrameMode(true);
             />
         </div>
     </div>
-    <?/*?><p class="template-topblock__content"><?echo $arResult["~DESCRIPTION"]; ?></p><?*/?>
     <p class="template-topblock__content"><?echo $arResult['COMMON_SECTION']['ITEMS'][10716]['PREVIEW_TEXT']; ?></p>
 </div>
 
@@ -85,40 +76,53 @@ $this->setFrameMode(true);
     });
 </script>
 
-<div>
-    <div>Пакет</div>
-    <? foreach ($arResult['SECTION_LEVEL_1'] as $section) : ?>
-        <a href="<?= $section['SECTION_PAGE_URL'] ?>"><?= $section['DESCRIPTION'] ?></a>
-    <? endforeach; ?>
-</div>
-
-<div>
-    <div>№ п/п</div>
-    <div>Вид операций и услуг</div>
-    <div>Пакет<br><? echo $arResult['SECTION']['PATH'][0]['~DESCRIPTION']; ?></div>
-</div>
-<? foreach ($arResult['TABLE'] as $key=>$arItems) : ?>
-    <div>
-        <div><?=$key+1?>.</div>
-        <div><?= $arItems['DESCRIPTION'] ?></div>
-        <div></div>
-        <? foreach ($arItems['ITEMS'] as $arItem) : ?>
-            <div><?= $arItem['PROPERTIES']['ATT_TABLE_LINE']['VALUE']; ?></div>
-            <? if ($arItem['PROPERTIES']['ATT_SERVICE_POINT']['VALUE']) : ?>
-                <div><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT']['~VALUE']; ?></div>
-                <div><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT']['~DESCRIPTION']; ?></div>
-            <? endif; ?>
-            <? if ($arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['VALUE']) : ?>
-                <? for ($kk=0; $kk<count($arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['~VALUE']); $kk++) : ?>
-                    <div><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['~VALUE'][$kk]; ?></div>
-                    <div><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['~DESCRIPTION'][$kk]; ?></div>
-                <? endfor; ?>
-            <? endif; ?>
+<div class="template-table">
+    <div class="table-choice">
+        <h4 class="table-choice--title">Пакет</h4>
+        <? foreach ($arResult['SECTION_LEVEL_1'] as $section) : ?>
+            <a class="table-choice--item" href="<?= $section['SECTION_PAGE_URL'] ?>"><?= $section['DESCRIPTION'] ?></a>
         <? endforeach; ?>
     </div>
-<? endforeach; ?>
+    <div class="table-wrapper">
+        <div class="table-top">
+            <h4 class="table-top--title table-cell-1">№ п/п</h4>
+            <h4 class="table-top--title table-cell-2">Вид операций и услуг</h4>
+            <h4 class="table-top--title table-cell-3">Пакет<br><? echo $arResult['SECTION']['PATH'][0]['~DESCRIPTION']; ?></h4>
+        </div>
+        <? foreach ($arResult['TABLE'] as $key=>$arItems) : ?>
+            <div class="table-box">
+                <div class="table-line">
+                    <div class="table-line--cell table-cell-1 table-cell--title"><?=$key+1?>.</div>
+                    <div class="table-line--cell table-cell-2 table-cell--title"><?= $arItems['DESCRIPTION'] ?></div>
+                    <div class="table-cell-3"></div>
+                </div>
+                <div class="table-subblock">
+                    <? foreach ($arItems['ITEMS'] as $arItem) : ?>
+                    <div class="table-line">
+                        <div class="table-line--cell table-cell-1"><?= $arItem['PROPERTIES']['ATT_TABLE_LINE']['VALUE']; ?></div>
+                        <? if ($arItem['PROPERTIES']['ATT_SERVICE_POINT']['VALUE']) : ?>
+                            <div class="table-line--cell table-cell-2"><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT']['~VALUE']; ?></div>
+                            <div class="table-line--cell table-cell-3"><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT']['~DESCRIPTION']; ?></div>
+                        <? endif; ?>
+                        <? if ($arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['VALUE']) : ?>
+                            <div class="table-point">
+                                <? for ($kk=0; $kk<count($arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['~VALUE']); $kk++) : ?>
+                                    <div class="table-subline">
+                                        <div class="table-line--cell table-cell-4"><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['~VALUE'][$kk]; ?></div>
+                                        <div class="table-line--cell table-cell-5"><?= $arItem['PROPERTIES']['ATT_SERVICE_POINT_COMPL']['~DESCRIPTION'][$kk]; ?></div>
+                                    </div>
+                                <? endfor; ?>
+                            </div>
+                        <? endif; ?>
+                    </div>
+                    <? endforeach; ?>
+                </div>
+            </div>
+        <? endforeach; ?>
+    </div>
+</div>
 
-<?foreach($arResult["ITEMS"] as $arItem):?>
+<?/* foreach($arResult["ITEMS"] as $arItem):?>
     <?
     $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
     $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
@@ -181,35 +185,8 @@ $this->setFrameMode(true);
         </small><br />
     <?endforeach;?>
     </div>
-<?endforeach;?>
+<?endforeach;*/?>
 
-<?/*?>
-<section class="consultations-tileblock">
-    <h3 class="consultations-tileblock__header"><?= $arResult["PROPERTY_HEADER"] ?></h3>
-    <div class="consultations-tileblock__grid">
-        <? foreach ($arResult["PROPERTIES"][$arParams['SERVICES_BLOCK'][0]] as $key=>$arItem) : ?>
-            <div class="consultations-tileblock__grid--item">
-                <div class="consultations-tileblock__grid--item-box item-box-<?= $key ?>">
-                    <div class="consultations-tileblock__grid--img">
-                        <img
-                                src="<?=CFile::GetPath($arItem["icon"])?>"
-                                alt="иконка"
-                                title="<?=$arItem["main"]?>"
-                        />
-                    </div>
-                    <h4 class="consultations-tileblock__grid--title"><?= $arItem["main"]; ?></h4>
-                    <p class="consultations-tileblock__grid--subtitle">
-                        <? if ($arItem["dop"]) : ?>
-                            <span><?= $arItem["dop"]; ?></span>
-                        <? endif; ?>
-                    </p>
-                </div>
-            </div>
-        <? endforeach; ?>
-    </div>
-</section>
-<?*/?>
-<??>
 <script>
     $(document).ready(function () {
         $('.js-base-account__button').on('click', function() {
@@ -229,4 +206,3 @@ $this->setFrameMode(true);
         );
     });
 </script>
-<??>
