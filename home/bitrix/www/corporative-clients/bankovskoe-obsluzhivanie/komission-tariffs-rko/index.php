77ba990bd9c95e493e264539a29b560d5c9d9623
andrew1762 <a.vanzha@tsbnk.ru>
2023-11-29 15:25:53 +0300

    tsb-62 изменение структуры страницы /komission-tariffs-rko/

--- home/bitrix/www/corporative-clients/bankovskoe-obsluzhivanie/komission-tariffs-rko/index.php
+++ home/bitrix/www/corporative-clients/bankovskoe-obsluzhivanie/komission-tariffs-rko/index.php
@@ -142,6 +142,7 @@ Asset::getInstance()->addCss("/corporative-clients/bankovskoe-obsluzhivanie/komi
 </div><!-- v21-section -->   index_block_maintext
 <?*/?>
 
+<?/*?>
 <div class="v21-section">
     <div class="v21-container">
         <?$APPLICATION->IncludeComponent(
@@ -155,7 +156,263 @@ Asset::getInstance()->addCss("/corporative-clients/bankovskoe-obsluzhivanie/komi
         );?>
     </div><!-- v21-container -->
 </div><!-- v21-section -->
+<?*/?>
 
+<section class="rko-doc">
+    <div class="v21-container">
+        <div class="row">
+            <div class="rko-doc__block rko-doc__rate col-md-4">
+                <header class="rko-doc__header">
+                    <div class="rko-doc__title">Тарифы</div>
+                </header>
+                <div class="rko-doc__items">
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:news.detail",
+                        "documents-type-1",
+                        Array(
+                            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                            "ADD_ELEMENT_CHAIN" => "N",
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "AJAX_MODE" => "N",
+                            "AJAX_OPTION_ADDITIONAL" => "",
+                            "AJAX_OPTION_HISTORY" => "N",
+                            "AJAX_OPTION_JUMP" => "N",
+                            "AJAX_OPTION_STYLE" => "Y",
+                            "BROWSER_TITLE" => "-",
+                            "CACHE_GROUPS" => "Y",
+                            "CACHE_TIME" => "36000000",
+                            "CACHE_TYPE" => "A",
+                            "CHECK_DATES" => "Y",
+                            "DETAIL_URL" => "",
+                            "DISPLAY_BOTTOM_PAGER" => "N",
+                            "DISPLAY_DATE" => "Y",
+                            "DISPLAY_NAME" => "Y",
+                            "DISPLAY_PICTURE" => "Y",
+                            "DISPLAY_PREVIEW_TEXT" => "Y",
+                            "DISPLAY_TOP_PAGER" => "N",
+                            "ELEMENT_CODE" => "",
+                            "ELEMENT_ID" => "8887",
+                            "FIELD_CODE" => array("", ""),
+                            "IBLOCK_ID" => "189",
+                            "IBLOCK_TYPE" => "ls_documents",
+                            "IBLOCK_URL" => "",
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                            "MESSAGE_404" => "",
+                            "META_DESCRIPTION" => "-",
+                            "META_KEYWORDS" => "-",
+                            "PAGER_BASE_LINK_ENABLE" => "N",
+                            "PAGER_SHOW_ALL" => "N",
+                            "PAGER_TEMPLATE" => ".default",
+                            "PAGER_TITLE" => "Страница",
+                            "PROPERTY_CODE" => array("", "DOCUMENTS", "CLASSES"),
+                            "SET_BROWSER_TITLE" => "N",
+                            "SET_CANONICAL_URL" => "N",
+                            "SET_LAST_MODIFIED" => "N",
+                            "SET_META_DESCRIPTION" => "N",
+                            "SET_META_KEYWORDS" => "N",
+                            "SET_STATUS_404" => "N",
+                            "SET_TITLE" => "N",
+                            "SHOW_404" => "N",
+                            "STRICT_SECTION_CHECK" => "N",
+                            "USE_PERMISSIONS" => "N",
+                            "USE_SHARE" => "N"
+                        )
+                    );?>
+                </div>
+            </div>
+            <div class="rko-doc__block rko-doc__contract offset-md-1 col-md-4">
+                <header class="rko-doc__header">
+                    <div class="rko-doc__title">Договоры</div>
+                </header>
+                <div class="rko-doc__items">
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:news.detail",
+                        "documents-type-1",
+                        Array(
+                            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                            "ADD_ELEMENT_CHAIN" => "N",
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "AJAX_MODE" => "N",
+                            "AJAX_OPTION_ADDITIONAL" => "",
+                            "AJAX_OPTION_HISTORY" => "N",
+                            "AJAX_OPTION_JUMP" => "N",
+                            "AJAX_OPTION_STYLE" => "Y",
+                            "BROWSER_TITLE" => "-",
+                            "CACHE_GROUPS" => "Y",
+                            "CACHE_TIME" => "36000000",
+                            "CACHE_TYPE" => "A",
+                            "CHECK_DATES" => "Y",
+                            "DETAIL_URL" => "",
+                            "DISPLAY_BOTTOM_PAGER" => "N",
+                            "DISPLAY_DATE" => "Y",
+                            "DISPLAY_NAME" => "Y",
+                            "DISPLAY_PICTURE" => "Y",
+                            "DISPLAY_PREVIEW_TEXT" => "Y",
+                            "DISPLAY_TOP_PAGER" => "N",
+                            "ELEMENT_CODE" => "",
+                            "ELEMENT_ID" => "8888",
+                            "FIELD_CODE" => array("", ""),
+                            "IBLOCK_ID" => "189",
+                            "IBLOCK_TYPE" => "ls_documents",
+                            "IBLOCK_URL" => "",
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                            "MESSAGE_404" => "",
+                            "META_DESCRIPTION" => "-",
+                            "META_KEYWORDS" => "-",
+                            "PAGER_BASE_LINK_ENABLE" => "N",
+                            "PAGER_SHOW_ALL" => "N",
+                            "PAGER_TEMPLATE" => ".default",
+                            "PAGER_TITLE" => "Страница",
+                            "PROPERTY_CODE" => array("", "DOCUMENTS", "CLASSES"),
+                            "SET_BROWSER_TITLE" => "N",
+                            "SET_CANONICAL_URL" => "N",
+                            "SET_LAST_MODIFIED" => "N",
+                            "SET_META_DESCRIPTION" => "N",
+                            "SET_META_KEYWORDS" => "N",
+                            "SET_STATUS_404" => "N",
+                            "SET_TITLE" => "N",
+                            "SHOW_404" => "N",
+                            "STRICT_SECTION_CHECK" => "N",
+                            "USE_PERMISSIONS" => "N",
+                            "USE_SHARE" => "N"
+                        )
+                    );?>
+                </div>
+            </div>
+            <div class="rko-doc__block rko-doc__push-bank col-12">
+                <header class="rko-doc__header">
+                    <div class="rko-doc__title">Документы для предоставления в Банк</div>
+                </header>
+                <div class="rko-doc__items row">
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:news.detail",
+                        "documents-type-1",
+                        Array(
+                            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                            "ADD_ELEMENT_CHAIN" => "N",
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "AJAX_MODE" => "N",
+                            "AJAX_OPTION_ADDITIONAL" => "",
+                            "AJAX_OPTION_HISTORY" => "N",
+                            "AJAX_OPTION_JUMP" => "N",
+                            "AJAX_OPTION_STYLE" => "Y",
+                            "BROWSER_TITLE" => "-",
+                            "CACHE_GROUPS" => "Y",
+                            "CACHE_TIME" => "36000000",
+                            "CACHE_TYPE" => "A",
+                            "CHECK_DATES" => "Y",
+                            "DETAIL_URL" => "",
+                            "DISPLAY_BOTTOM_PAGER" => "N",
+                            "DISPLAY_DATE" => "Y",
+                            "DISPLAY_NAME" => "Y",
+                            "DISPLAY_PICTURE" => "Y",
+                            "DISPLAY_PREVIEW_TEXT" => "Y",
+                            "DISPLAY_TOP_PAGER" => "N",
+                            "ELEMENT_CODE" => "",
+                            "ELEMENT_ID" => "8889",
+                            "FIELD_CODE" => array("", ""),
+                            "IBLOCK_ID" => "189",
+                            "IBLOCK_TYPE" => "ls_documents",
+                            "IBLOCK_URL" => "",
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                            "MESSAGE_404" => "",
+                            "META_DESCRIPTION" => "-",
+                            "META_KEYWORDS" => "-",
+                            "PAGER_BASE_LINK_ENABLE" => "N",
+                            "PAGER_SHOW_ALL" => "N",
+                            "PAGER_TEMPLATE" => ".default",
+                            "PAGER_TITLE" => "Страница",
+                            "PROPERTY_CODE" => array("", "DOCUMENTS", "CLASSES"),
+                            "SET_BROWSER_TITLE" => "N",
+                            "SET_CANONICAL_URL" => "N",
+                            "SET_LAST_MODIFIED" => "N",
+                            "SET_META_DESCRIPTION" => "N",
+                            "SET_META_KEYWORDS" => "N",
+                            "SET_STATUS_404" => "N",
+                            "SET_TITLE" => "N",
+                            "SHOW_404" => "N",
+                            "STRICT_SECTION_CHECK" => "N",
+                            "USE_PERMISSIONS" => "N",
+                            "USE_SHARE" => "N"
+                        )
+                    );?>
+                </div>
+            </div>
+            <div class="rko-doc__block rko-doc__client-bank col-12">
+                <header class="rko-doc__header">
+                    <div class="rko-doc__title">Сертификаты и руководства пользователя системы Клиент-Банк</div>
+                </header>
+                <div class="rko-doc__items row">
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:news.detail",
+                        "documents-type-1",
+                        Array(
+                            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                            "ADD_ELEMENT_CHAIN" => "N",
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "AJAX_MODE" => "N",
+                            "AJAX_OPTION_ADDITIONAL" => "",
+                            "AJAX_OPTION_HISTORY" => "N",
+                            "AJAX_OPTION_JUMP" => "N",
+                            "AJAX_OPTION_STYLE" => "Y",
+                            "BROWSER_TITLE" => "-",
+                            "CACHE_GROUPS" => "Y",
+                            "CACHE_TIME" => "36000000",
+                            "CACHE_TYPE" => "A",
+                            "CHECK_DATES" => "Y",
+                            "DETAIL_URL" => "",
+                            "DISPLAY_BOTTOM_PAGER" => "N",
+                            "DISPLAY_DATE" => "Y",
+                            "DISPLAY_NAME" => "Y",
+                            "DISPLAY_PICTURE" => "Y",
+                            "DISPLAY_PREVIEW_TEXT" => "Y",
+                            "DISPLAY_TOP_PAGER" => "N",
+                            "ELEMENT_CODE" => "",
+                            "ELEMENT_ID" => "8890",
+                            "FIELD_CODE" => array("", ""),
+                            "IBLOCK_ID" => "189",
+                            "IBLOCK_TYPE" => "ls_documents",
+                            "IBLOCK_URL" => "",
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                            "MESSAGE_404" => "",
+                            "META_DESCRIPTION" => "-",
+                            "META_KEYWORDS" => "-",
+                            "PAGER_BASE_LINK_ENABLE" => "N",
+                            "PAGER_SHOW_ALL" => "N",
+                            "PAGER_TEMPLATE" => ".default",
+                            "PAGER_TITLE" => "Страница",
+                            "PROPERTY_CODE" => array("", "DOCUMENTS", "CLASSES"),
+                            "SET_BROWSER_TITLE" => "N",
+                            "SET_CANONICAL_URL" => "N",
+                            "SET_LAST_MODIFIED" => "N",
+                            "SET_META_DESCRIPTION" => "N",
+                            "SET_META_KEYWORDS" => "N",
+                            "SET_STATUS_404" => "N",
+                            "SET_TITLE" => "N",
+                            "SHOW_404" => "N",
+                            "STRICT_SECTION_CHECK" => "N",
+                            "USE_PERMISSIONS" => "N",
+                            "USE_SHARE" => "N"
+                        )
+                    );?>
+                </div>
+            </div>
+        </div>
+        <div class="rko-doc__all">
+            <a href="/arkhiv-tarifov-i-dokumentov/" class="rko-doc__all--link-button">
+                <span>Архив тарифов и документов</span>
+            </a>
+            <a href="/arkhiv-tarifov-i-dokumentov/" target="_blank" class="rko-doc__all--link-details">
+                <!--span>Подробнее </span-->
+                <svg class="rko-doc__all--link-arrow" xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
+                    <path d="M13.2371 0.989515C13.2371 0.492459 12.8342 0.0895151 12.3371 0.0895145L4.23715 0.0895146C3.74009 0.0895146 3.33714 0.492459 3.33714 0.989515C3.33714 1.48657 3.74009 1.88952 4.23714 1.88951L11.4371 1.88951L11.4371 9.08952C11.4371 9.58657 11.8401 9.98952 12.3371 9.98952C12.8342 9.98952 13.2371 9.58657 13.2371 9.08951L13.2371 0.989515ZM1.65983 12.9396L12.9735 1.62591L11.7007 0.353119L0.387041 11.6668L1.65983 12.9396Z" fill="#00345E"/>
+                </svg>
+            </a>
+        </div>
+    </div><!-- v21-container -->
+</section><!-- v21-section -->
+
+<?/*?>
 <div class="v21-section">
     <div class="v21-wide-container v21-documents-vkontrol-container">
         <div class="v21-container">
@@ -249,6 +506,7 @@ Asset::getInstance()->addCss("/corporative-clients/bankovskoe-obsluzhivanie/komi
     </div><!-- v21-container -->
 
 </div><!-- v21-section -->
+<?*/?>
 
 <div class="">
     <div class="">
