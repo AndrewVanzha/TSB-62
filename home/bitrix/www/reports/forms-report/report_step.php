935ad0fc015b624919f4e70f179f20d0e94062b6
andrew1762 <a.vanzha@tsbnk.ru>
2023-12-14 20:00:33 +0300

    tsb-62 редакция /reports/forms-report/report_step.php

--- home/bitrix/www/reports/forms-report/report_step.php
+++ home/bitrix/www/reports/forms-report/report_step.php
@@ -22,14 +22,14 @@ $generated_xls_php = $_SERVER["DOCUMENT_ROOT"] . '/reports/sale_report/generated
 <?
 debugg($_POST);
 $has_checkbox = false;
-$arFormList = [];
+$iblockID_list = [];
 foreach ($_POST as $key=>$item) {
     if (stripos($key, 'CHECKBOX_') !== false) {
         //debugg($key);
         //debugg($item);
         $has_checkbox = true;
         if ($item == 'on') {
-            $arFormList[] = str_replace('CHECKBOX_', '', $key);
+            $iblockID_list[] = str_replace('CHECKBOX_', '', $key);
         }
     }
 }
@@ -39,24 +39,60 @@ if (!$has_checkbox) {
         <a href="admin_step.php" class="adm-btn adm-btn-save">Создать новый отчет</a>
     <?php
 } else {
-    debugg($arFormList);
-    ;
-}
+    debugg($iblockID_list);
+    if (isset($_POST['dateFrom'])) {
+        $dateFrom = $_POST['dateFrom'];
+    } else {
+        $objDateTime = new DateTime("2023-01-01 00:00:00", "Y-m-d H:i:s");
+        $dateFrom = $objDateTime->format('d.m.Y');
+    }
+    $arFormElements = [];
+    $props = CIBlockElement::GetList (
+        Array("IBLOCK_ID" => "ASC"),
+        Array("IBLOCK_ID" => $iblockID_list, '>=DATE_CREATE' => $dateFrom),
+        //Array("IBLOCK_ID" => '15', '>=DATE_CREATE' => $dateFrom),
+        false,
+        false,
+        //Array(),
+        Array('IBLOCK_ID', 'ID', 'NAME', 'DATE_CREATE', 'IBLOCK_NAME'),
+    );
+    while ($ar_fields = $props->GetNextElement()) {
+        $ar_props = [];
+        $ar_element = $ar_fields->GetFields();
+        $ar_get_prop = $ar_fields->GetProperties();
+        foreach ($ar_get_prop as $key=>$property) {
+            $ar_props[$key]['ID'] = $property['ID'];
+            $ar_props[$key]['IBLOCK_ID'] = $property['IBLOCK_ID'];
+            $ar_props[$key]['NAME'] = $property['NAME'];
+            $ar_props[$key]['VALUE'] = $property['VALUE'];
+        }
+        $ar_element['PROPERTIES'] = $ar_props;
+        //debugg($ar_element);
+        //debugg($ar_fields->GetProperties());
+        //debugg($ar_props);
+        $arFormElements[] = $ar_element;
+        unset($ar_props);
+    }
+    //debugg($arFormElements);
 
-if (!empty($_POST['lastOrderId'])) {
-    $arFilter = array(
-        ">=DATE_INSERT" => $_POST['dateFrom'],
-        "<=DATE_INSERT" => $_POST['dateTo'],
-        ">ID" => $_POST['lastOrderId']
-    );
-} else {
-    $arFilter = array(
-        ">=DATE_INSERT" => $_POST['dateFrom'],
-        "<=DATE_INSERT" => $_POST['dateTo']
-    );
+    $arResultList = [];
+    for ($ii=0; $ii<count($iblockID_list); $ii++) {
+        foreach ($arFormElements as $item) {
+            if ($iblockID_list[$ii] == $item['IBLOCK_ID']) {
+                $i_block_list['ID'] = $item['IBLOCK_ID'];
+                $i_block_list['DATE_CREATE'] = $item['DATE_CREATE'];
+                $i_block_list['NAME'] = $item['NAME'];
+                $i_block_list['IBLOCK_NAME'] = $item['IBLOCK_NAME'];
+                $i_block_list['PROPERTIES'] = $item['PROPERTIES'];
+                $arResultList[$item['IBLOCK_ID']][] = $i_block_list;
+            }
+        }
+    }
+    debugg($arResultList);
+    file_put_contents("/home/bitrix/www".'/logs/a_$arResultList.json', json_encode($arResultList));
+
     $vfile = file_put_contents($generated_xls_php, '');
-    //file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/reports/a_test.json', json_encode($generated_xls_php));
-
+    //file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/logs/a_test.json', json_encode($generated_xls_php));
     /*$fileHeader = '<?
 Header("Content-Type: application/force-download");
 Header("Content-Type: application/octet-stream");
@@ -96,6 +132,99 @@ Header("Content-Transfer-Encoding: binary");
         <td>Цена товара</td>
         <td>Наименование товара</td>
     </tr>';
+    $vfile = file_put_contents($generated_xls_php, $fileHeader, FILE_APPEND);
+
+    // формирую вывод в excel-таблицу
+    $orderData = '';
+    $arOrders = [];
+    foreach ($arOrders as $order) {
+        $orderData = '<tr>
+                    <td>' . $order['DATE_INSERT_FORMAT'] . '</td>
+                    <td>' . $order['ID'] . '</td>
+                    <td>' . $order['PRICE'] . '</td>
+                    <td>' . $order['USER_NAME'] . ' ' . $order['USER_LAST_NAME'] . '</td>
+                    <td>' . $order['USER_EMAIL'] . '</td>
+                    <td>' . $order['STATUS'] . '</td>
+                    <td>' . $order['SHIPMENT'][0] . '</td>';
+        foreach ($order['ORDER'] as $kk=>$item) {
+            if (count($order['ORDER'])>1 && $kk>0) {
+                $orderData .= '<tr>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>';
+            }
+            $orderData .=  '<td>' . $item['PRODUCT']['ARTICLE_VALUE'] . '</td>
+                        <td>' . $item['QUANTITY'] . '</td>
+                        <td>' . $item['PRICE'] . '</td>
+                        <td>' . $item['NAME'] . '</td>
+                      </tr>';
+        }
+        file_put_contents($generated_xls_php, $orderData, FILE_APPEND);
+    }
+    file_put_contents($generated_xls_php, '</table></body></html>', FILE_APPEND);
+    ?>
+    <a href="generated.xls" class="adm-btn adm-btn-save" download>Скачать отчет</a>
+    <a href="admin_step.php" class="adm-btn adm-btn-save">Создать новый отчет</a>
+    <?php
+}
+
+if (!empty($_POST['lastOrderId'])) {
+    $arFilter = array(
+        ">=DATE_INSERT" => $_POST['dateFrom'],
+        "<=DATE_INSERT" => $_POST['dateTo'],
+        ">ID" => $_POST['lastOrderId']
+    );
+} else {
+    $arFilter = array(
+        ">=DATE_INSERT" => $_POST['dateFrom'],
+        "<=DATE_INSERT" => $_POST['dateTo']
+    );
+    //$vfile = file_put_contents($generated_xls_php, '');
+    //file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/reports/a_test.json', json_encode($generated_xls_php));
+
+    /*$fileHeader = '<?
+Header("Content-Type: application/force-download");
+Header("Content-Type: application/octet-stream");
+Header("Content-Type: application/download");
+Header("Content-Disposition: attachment;filename=excel_orders.xls");
+Header("Content-Transfer-Encoding: binary");
+?>*/
+    /*$fileHeader = '<?
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <style>
+        td {
+            mso-number-format: \@;
+        }
+        .number0 {
+            mso-number-format: 0;
+        }
+        .number2 {
+            mso-number-format: Fixed;
+        }
+    </style>
+</head>
+<body>
+
+<table border="1">
+    <tr>
+        <td>Дата</td>
+        <td>ID заказа</td>
+        <td>Сумма</td>
+        <td>ФИО покупателя</td>
+        <td>Email покупателя</td>
+        <td>Статус</td>
+        <td>Доставка</td>
+        <td>Артикул товара</td>
+        <td>Количество товара</td>
+        <td>Цена товара</td>
+        <td>Наименование товара</td>
+    </tr>';*/
     /*
     <tr>
         <td>ID заказа</td>
@@ -117,7 +246,7 @@ Header("Content-Transfer-Encoding: binary");
         <td>Заказ: Заказ отменён</td>
     </tr>';
      */
-    $vfile = file_put_contents($generated_xls_php, $fileHeader, FILE_APPEND);
+    //$vfile = file_put_contents($generated_xls_php, $fileHeader, FILE_APPEND);
 }
 $ii = 0;
 $arOrders = [];
@@ -314,7 +443,7 @@ foreach ($arOrder as $order) {
 }
 //echo '<pre>';print_r($arOrder);echo '</pre>';
 */
-
+/*
 // формирую вывод в excel-таблицу
 $orderData = '';
 foreach ($arOrders as $order) {
@@ -346,7 +475,7 @@ foreach ($arOrders as $order) {
     file_put_contents($generated_xls_php, $orderData, FILE_APPEND);
 }
 file_put_contents($generated_xls_php, '</table></body></html>', FILE_APPEND);
-
+*/
 /*
 if ($ii > 1) { // пошаговый ?>
     <form action="" method="post" id="postStep" style="display: none;">
@@ -376,8 +505,9 @@ if ($ii > 1) { // пошаговый ?>
     print_r($vfile);
     //LocalRedirect('generated.xls.php');
 }*/
+/*
 ?>
     <a href="generated.xls" class="adm-btn adm-btn-save" download>Скачать отчет</a>
     <a href="admin_step.php" class="adm-btn adm-btn-save">Создать новый отчет</a>
-<?php
+<?php*/
 require($_SERVER["DOCUMENT_ROOT"] . BX_ROOT . "/modules/main/include/epilog_admin.php"); ?>
